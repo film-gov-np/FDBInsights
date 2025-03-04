d5ff8226e4a9e1c9de3fc7ec8577dd2d92d0a07a
Sushil Shrestha <shoesheill@gmail.com>
2025-03-04 23:59:33 +0545

    feat(handlers): implement LoginCommandHandler with IUserRepository for user authentication

--- api/FDBInsights/Features/Auth/Commands/AuthCommandHandler.cs
+++ api/FDBInsights/Features/Auth/Commands/AuthCommandHandler.cs
@@ -1,12 +1,21 @@
 using ErrorOr;
+using FDBInsights.Repositories;
 using MediatR;
 
 namespace FDBInsights.Features.Auth.Commands;
 
 public class LoginCommandHandler : IRequestHandler<LoginCommand, ErrorOr<LoginResponse>>
 {
-    public Task<ErrorOr<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    private readonly IUserRepository _userRepository;
+
+    public LoginCommandHandler(IUserRepository userRepository)
     {
+        _userRepository = userRepository;
+    }
+
+    public async Task<ErrorOr<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByEmailAsync(request.Email);
         throw new NotImplementedException();
     }
 }
