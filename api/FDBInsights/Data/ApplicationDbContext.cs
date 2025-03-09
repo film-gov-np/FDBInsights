11558cf8799dcdda9140c8e10c8012da494866a3
Sushil Shrestha <shoesheill@gmail.com>
2025-03-09 09:41:27 +0545

    refactor(db): simplify ApplicationDbContext constructor

--- api/FDBInsights/Data/ApplicationDbContext.cs
+++ api/FDBInsights/Data/ApplicationDbContext.cs
@@ -1,12 +1,9 @@
+using FDBInsights.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FDBInsights.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-    }
-
     public DbSet<User> Users { get; set; }
 }
