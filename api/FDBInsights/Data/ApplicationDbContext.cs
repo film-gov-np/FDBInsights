39950735451c7e14d5558ca6dbd53824109d924e
Sushil Shrestha <shoesheill@gmail.com>
2025-03-11 23:20:49 +0545

    feat(database): add UserRole DbSet and configure UserRole entity in ApplicationDbContext

--- api/FDBInsights/Data/ApplicationDbContext.cs
+++ api/FDBInsights/Data/ApplicationDbContext.cs
@@ -6,4 +6,11 @@ namespace FDBInsights.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRole { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UserRole>().HasNoKey();
+    }
 }
