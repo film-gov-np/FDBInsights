3d45c57e4cc8842c4b2a050a6f19ce0b43bcfb5c
Sushil Shrestha <shoesheill@gmail.com>
2025-03-15 18:38:12 +0545

    fix(apiResponse): change default Message value to an empty string

--- api/FDBInsights/Common/ApiResponse.cs
+++ api/FDBInsights/Common/ApiResponse.cs
@@ -2,7 +2,7 @@ namespace FDBInsights.Common;
 
 public record ApiResponse<T>(
     T? Data,
-    string? Message = null,
+    string? Message = "",
     int StatusCode = 200,
     bool Success = true
 );
