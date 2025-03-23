aec92390b228627d855aaeab1802c16663a85955
sudip <diabolic_kaji@hotmail.com>
2025-03-23 15:41:43 +0545

    app js commit

--- nmdb-insights-web/src/App.jsx
+++ nmdb-insights-web/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import LoginForm from "./components/ui/login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <LoginForm />
